a57dccf3cb36107d30367f39f2f9609077009891
NishantJha958 <your-email@example.com>
2026-05-29 00:58:01 +0530

    add user and video model

--- src/utils/apiErrors.js
+++ src/utils/apiErrors.js
@@ -2,12 +2,11 @@ class ApiError extends Error {
     constructor(
         statusCode,
         message = "SOmething went wrong",
-        error = [],
+        errors = [],
         stack = ""
     ) {
         super(message)
         this.statusCode = statusCode
-        this.error = error
         this.success = false
         this.data = null;
         this.errors = errors
